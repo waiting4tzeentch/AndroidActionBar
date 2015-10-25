cce43c9406d23ea4d12adabda741803c1c8f1299
Michael Moser <michael.opper.moser@gmail.com>
2015-10-25 15:37:12 -0400

    added MainFragment java file

---
+++ app/src/main/java/com/moser/michael/androidactionbar/MainFragment.java
@@ -0,0 +1,20 @@
+package com.moser.michael.androidactionbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by michaelmoser on 10/25/15.
+ */
+
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        return view;
+    }
+}
